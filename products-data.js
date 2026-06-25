c8ffc7162f690bc76f3e3eeb8f5deae6115aecb5
paladinsdynasty <info.dynaframestudios@gmail.com>
2026-06-25 08:58:33 +0200

    commits

---
+++ products-data.js
@@ -0,0 +1,182 @@
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    battery: "8 hours",
+    image: "image1.png",
+    colors: ["#000000", "#FFFFFF", "#1A1A1A"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "pods"]
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    battery: "10 hours",
+    image: "image2.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "earphones"]
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    battery: "12 hours",
+    image: "image3.png",
+    colors: ["#000000", "#FFFFFF", "#333333"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["battery", "pods"]
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    battery: "9 hours",
+    image: "image4.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["aux", "earphones"]
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    battery: "11 hours",
+    image: "image5.png",
+    colors: ["#000000", "#FFFFFF", "#1A1A1A"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "pods"]
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    battery: "6 hours",
+    image: "image6.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "earphones"]
+  },
+  {
+    id: 7,
+    name: "Product 7",
+    battery: "15 hours",
+    image: "image7.png",
+    colors: ["#000000", "#FFFFFF", "#333333"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["battery", "pods"]
+  },
+  {
+    id: 8,
+    name: "Product 8",
+    battery: "7 hours",
+    image: "image8.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["aux", "earphones"]
+  },
+  {
+    id: 9,
+    name: "Product 9",
+    battery: "13 hours",
+    image: "image9.png",
+    colors: ["#000000", "#FFFFFF", "#1A1A1A"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "pods"]
+  },
+  {
+    id: 10,
+    name: "Product 10",
+    battery: "10 hours",
+    image: "image10.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "earphones"]
+  },
+  {
+    id: 11,
+    name: "Product 11",
+    battery: "9 hours",
+    image: "image11.png",
+    colors: ["#000000", "#FFFFFF", "#333333"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["battery", "pods"]
+  },
+  {
+    id: 12,
+    name: "Product 12",
+    battery: "8 hours",
+    image: "image12.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["aux", "earphones"]
+  },
+  {
+    id: 13,
+    name: "Product 13",
+    battery: "16 hours",
+    image: "image13.png",
+    colors: ["#000000", "#FFFFFF", "#1A1A1A"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "pods"]
+  },
+  {
+    id: 14,
+    name: "Product 14",
+    battery: "6 hours",
+    image: "image14.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "earphones"]
+  },
+  {
+    id: 15,
+    name: "Product 15",
+    battery: "14 hours",
+    image: "image15.png",
+    colors: ["#000000", "#FFFFFF", "#333333"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["battery", "pods"]
+  },
+  {
+    id: 16,
+    name: "Product 16",
+    battery: "11 hours",
+    image: "image16.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["aux", "earphones"]
+  },
+  {
+    id: 17,
+    name: "Product 17",
+    battery: "12 hours",
+    image: "image17.png",
+    colors: ["#000000", "#FFFFFF", "#1A1A1A"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "pods"]
+  },
+  {
+    id: 18,
+    name: "Product 18",
+    battery: "9 hours",
+    image: "image18.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["bluetooth", "earphones"]
+  },
+  {
+    id: 19,
+    name: "Product 19",
+    battery: "15 hours",
+    image: "image19.png",
+    colors: ["#000000", "#FFFFFF", "#333333"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["battery", "pods"]
+  },
+  {
+    id: 20,
+    name: "Product 20",
+    battery: "7 hours",
+    image: "image20.png",
+    colors: ["#000000", "#FFFFFF"],
+    features: ["Feature 1", "Feature 2", "Feature 3"],
+    tags: ["aux", "earphones"]
+  }
+];
